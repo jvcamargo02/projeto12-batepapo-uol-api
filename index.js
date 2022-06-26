5ea088c165a5c2a8b5ec2e4c8054a510f556f58f
jvcamargo02 <joaovcamargo2002@gmail.com>
2022-06-26 15:54:54 -0300

    Feat: participants post && get verb implementation

--- index.js
+++ index.js
@@ -1,13 +1,35 @@
 import express from 'express'
 import cors from 'cors'
 import { MongoClient } from 'mongodb'
+import dotenv from 'dotenv'
+import joi from 'joi'
+import dayjs from "dayjs";
+import "dayjs/locale/pt-br.js";
 
+dotenv.config()
 const app = express()
 app.use(cors())
 app.use(express.json())
 
-const dbClient = new MongoClient("mongodb://127.0.0.1:27017");
+const dbClient = new MongoClient(process.env.MONGO_URL);
 let db;
+
+/* JOI SCHEMAS */
+const userSchema = joi.object({
+    name: joi.string().required()
+});
+
+
+function dataBaseConnect() {
+
+    dbClient.connect()
+    db = dbClient.db("BatePapo_UOL")
+}
+
+function dataBaseDisconnect() {
+
+    dbClient.close()
+}
 
 /* Se conecta ao banco dbClient.connect() */
 /* Se conecta ao documento dbClient.db("BatePapo_UOL") */
@@ -15,21 +37,60 @@ let db;
 /* Retorna o conteudo do banco db.collection('users').find().toArray()*/
 
 
-app.get('/', async (req, res) => {
+app.post("/participants", async (req, res) => {
 
-    await dbClient.connect()
-    db = dbClient.db("BatePapo_UOL")
+    const { name } = req.body
+    const validation = userSchema.validate({ name })
+    const users = await db.collection("users").find({ name }).toArray();
+    const reqTime = dayjs().locale('pt-br').format("HH:mm:ss");
+    
+    dataBaseConnect()
 
-    await db.collection('users').insertOne({
-        name: "joão4"
-    })
+    /* Valida String */
+    if ((validation.error) || !(isNaN(parseInt(name)))) {
+        return res.status(422).send("Confira o seu nome")
+    }
 
-    const users = await db.collection('users').find().toArray()
-    console.log(users)
+    /* Valida username disponivel */
+    if (users.length !== 0) {
+        return res.status(409).send("Este username já está em uso")
+    }
 
-    res.send(users).status(201)
+
+    try {
+        await db.collection("users").insertOne({
+            name,
+            lastStatus: Date.now()
+        })
+
+        await db.collection("messages").insertOne({
+            from: name,
+            to: 'Todos',
+            text: 'entra na sala...',
+            type: 'status',
+            time: reqTime
+        })
+
+        res.status(201).send("Usuário Logado")
+    } catch {
+        res.sendStatus(500)
+    }
+
+    dataBaseDisconnect()
+
 })
 
-app.listen("5000", () => {
-    console.log("Servidor em andamento")
+app.get("/participants", async (req, res) => {
+    
+    dataBaseConnect()
+
+    const users = await db.collection("users").find().toArray()
+
+    res.status(200).send(users)
+})
+
+
+
+app.listen(process.env.SERVER_PORT, () => {
+    console.log("Servidor ON")
 })
