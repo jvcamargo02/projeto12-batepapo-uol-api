a7a8d9af1c5b11d738fbc8e4cca8ab1cefa96920
jvcamargo02 <joaovcamargo2002@gmail.com>
2022-06-29 09:20:40 -0300

    Feat: remove offline users && Fix: get messages without limit

--- front-bate-papo-uol-main/assets/javascript/script.js
+++ front-bate-papo-uol-main/assets/javascript/script.js
@@ -33,7 +33,7 @@ function entrarNaSala() {
 function carregarMensagens() {
   if (!buscarMensagens) return;
 
-  const requisicao = axios.get("http://localhost:5000/messages?limit=4", {
+  const requisicao = axios.get("http://localhost:5000/messages", {
     headers: {
       User: nome
     }
--- index.js
+++ index.js
@@ -5,12 +5,12 @@ import dotenv from 'dotenv'
 import joi from 'joi'
 import dayjs from "dayjs";
 import "dayjs/locale/pt-br.js";
-import { text } from 'stream/consumers'
 
 dotenv.config()
 const app = express()
 app.use(cors())
 app.use(express.json())
+setInterval(offUsersRemove, 5000)
 
 const dbClient = new MongoClient(process.env.MONGO_URL);
 let db;
@@ -42,6 +42,24 @@ function dataBaseConnect() {
 function dataBaseDisconnect() {
 
     dbClient.close()
+}
+
+async function offUsersRemove() {
+
+    dataBaseConnect()
+    const dateNow = Date.now()
+    const users = await db.collection("users").find().toArray()
+
+    console.log(users)
+
+    users.map(user => {
+
+        if ((dateNow - user.lastStatus) > 10000) {
+            const userId = user._id;
+            const remove = db.collection("users").deleteOne({ _id: userId })
+            return remove
+        }
+    })
 }
 
 /* Se conecta ao banco dbClient.connect() */
@@ -142,32 +160,40 @@ app.post("/messages", async (req, res) => {
 })
 
 app.get("/messages", async (req, res) => {
-
+ 
     dataBaseConnect()
 
     const limitPrintMessage = parseInt(req.query.limit)
     const messages = await db.collection("messages").find().toArray()
     const { user } = req.headers
+    const userMessages = []
 
-    if (!(isNaN(limitPrintMessage)) && (limitPrintMessage < messages.length) && limitPrintMessage !== 0) {
+    messages.map((message) => {
 
-        let printMessages = []
-        const messageNumber = messages.length
-        const printMessagesNumber = messageNumber - limitPrintMessage
+        if ((message.type === "message") || (message.type === "status") || (message.to === user) || (message.from === user)) {
+            userMessages.push(message)
+        }})
 
-        for (let i = messageNumber; i > printMessagesNumber; i = i - 1) {
+        if (!(isNaN(limitPrintMessage)) && (limitPrintMessage < userMessages.length) && limitPrintMessage !== 0) {
 
-            if ((messages[i - 1].type === "message") || (messages[i - 1].to === user) || (messages[i - 1].from === user)) {
-                printMessages.unshift(messages[i - 1])
+            let printMessages = []
+            const messageNumber = userMessages.length
+            const printMessagesNumber = messageNumber - limitPrintMessage
+
+            for (let i = messageNumber; i > printMessagesNumber; i = i - 1) {
+
+                if ((userMessages[i - 1].type === "message") || (userMessages[i - 1].to === user) || (userMessages[i - 1].from === user)) {
+                    printMessages.unshift(userMessages[i - 1])
+                }
             }
+
+            return res.send(printMessages)
         }
 
-        return res.send(printMessages)
-    }
+        res.send(userMessages)
 
-    res.send(messages)
+    })
 
-})
 
 app.post("/status", async (req, res) => {
 
@@ -175,7 +201,8 @@ app.post("/status", async (req, res) => {
 
     const { user } = req.headers
     const userFind = await db.collection("users").find({
-        name: user}).toArray()
+        name: user
+    }).toArray()
 
     if (userFind.length === 0) {
         res.status(404).send()
@@ -183,7 +210,7 @@ app.post("/status", async (req, res) => {
 
     try {
         const userId = userFind[0]._id
-        const update = await db.collection("users").updateOne({ _id: userId }, { $set: { lastStatus: Date.now() }  })
+        const update = await db.collection("users").updateOne({ _id: userId }, { $set: { lastStatus: Date.now() } })
 
         res.status(200).send(update)
     } catch {
